3e38c7fa440e4b7660440e1cc7d169df403eed5c
ccaniano15 <33699343+ccaniano15@users.noreply.github.com>
2017-12-01 13:50:36 -0500

    Add files via upload

---
+++ college.js
@@ -0,0 +1,7 @@
+alert("working")
+
+document.getElementById("#amherst").addEventListener("click", function() {
+    // display the current click count inside the clicked div
+  document.getElementById("#collegeAmherst").style.visibility = "visible";
+
+  }, false);
