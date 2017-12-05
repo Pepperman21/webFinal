4bdd6a9a5868b3b761e30c270d11c68b2dc06e15
ccaniano15 <33699343+ccaniano15@users.noreply.github.com>
2017-12-05 15:32:42 -0500

    Add files via upload

--- college.js
+++ college.js
@@ -1,7 +1,120 @@
 alert("working")
 
-document.getElementById("#amherst").addEventListener("click", function() {
-    // display the current click count inside the clicked div
-  document.getElementById("#collegeAmherst").style.visibility = "visible";
+document.getElementById("amherst").addEventListener("click", function() {
+  	document.getElementById("collegeAmherst").className = "visible";
+    document.getElementById("amherst").classList.add("pageSelectTrue")
+    document.getElementById("amherst").classList.remove("hovering")
+
+  	document.getElementById("collegeHampshire").className = "notVisible";
+    document.getElementById("hampshire").classList.remove("pageSelectTrue")
+    document.getElementById("hampshire").classList.add("hovering")
+
+  	document.getElementById("collegeHolyoke").className = "notVisible";
+    document.getElementById("holyoke").classList.remove("pageSelectTrue")
+    document.getElementById("holyoke").classList.add("hovering")
+
+  	document.getElementById("collegeSmith").className = "notVisible";
+    document.getElementById("smith").classList.remove("pageSelectTrue")
+    document.getElementById("smith").classList.add("hovering")
+
+  	document.getElementById("collegeUMass").className = "notVisible";
+    document.getElementById("umass").classList.remove("pageSelectTrue")
+    document.getElementById("umass").classList.add("hovering")
+
+  }, false);
+
+document.getElementById("hampshire").addEventListener("click", function() {
+  	document.getElementById("collegeHampshire").className = "visible";
+    document.getElementById("hampshire").classList.add("pageSelectTrue")
+    document.getElementById("hampshire").classList.remove("hovering")
+
+
+  	document.getElementById("collegeHolyoke").className = "notVisible";
+    document.getElementById("holyoke").classList.remove("pageSelectTrue")
+    document.getElementById("holyoke").classList.add("hovering")
+
+
+  	document.getElementById("collegeSmith").className = "notVisible";
+    document.getElementById("smith").classList.remove("pageSelectTrue")
+    document.getElementById("smith").classList.add("hovering")
+
+
+  	document.getElementById("collegeUMass").className = "notVisible";
+    document.getElementById("umass").classList.remove("pageSelectTrue")
+    document.getElementById("umass").classList.add("hovering")
+
+
+  	document.getElementById("collegeAmherst").className = "notVisible";
+    document.getElementById("amherst").classList.remove("pageSelectTrue")
+    document.getElementById("amherst").classList.add("hovering")
+
+  }, false);
+
+document.getElementById("holyoke").addEventListener("click", function() {
+  	document.getElementById("collegeHampshire").className = "notVisible";
+    document.getElementById("hampshire").classList.remove("pageSelectTrue")
+    document.getElementById("hampshire").classList.add("hovering")
+
+  	document.getElementById("collegeHolyoke").className = "visible";
+    document.getElementById("holyoke").classList.add("pageSelectTrue")
+    document.getElementById("holyoke").classList.remove("hovering")
+
+  	document.getElementById("collegeSmith").className = "notVisible";
+    document.getElementById("smith").classList.remove("pageSelectTrue")
+    document.getElementById("smith").classList.add("hovering")
+
+  	document.getElementById("collegeUMass").className = "notVisible";
+    document.getElementById("umass").classList.remove("pageSelectTrue")
+    document.getElementById("umass").classList.add("hovering")
+
+  	document.getElementById("collegeAmherst").className = "notVisible";
+    document.getElementById("amherst").classList.remove("pageSelectTrue")
+    document.getElementById("amherst").classList.add("hovering")
+
+  }, false);
+
+document.getElementById("smith").addEventListener("click", function() {
+    document.getElementById("collegeHampshire").className = "notVisible";
+    document.getElementById("hampshire").classList.remove("pageSelectTrue")
+    document.getElementById("hampshire").classList.add("hovering")
+
+    document.getElementById("collegeHolyoke").className = "visible";
+    document.getElementById("holyoke").classList.remove("pageSelectTrue")
+    document.getElementById("holyoke").classList.add("hovering")
+
+    document.getElementById("collegeSmith").className = "notVisible";
+    document.getElementById("smith").classList.add("pageSelectTrue")
+    document.getElementById("smith").classList.remove("hovering")
+
+    document.getElementById("collegeUMass").className = "notVisible";
+    document.getElementById("umass").classList.remove("pageSelectTrue")
+    document.getElementById("umass").classList.add("hovering")
+
+    document.getElementById("collegeAmherst").className = "notVisible";
+    document.getElementById("amherst").classList.remove("pageSelectTrue")
+    document.getElementById("amherst").classList.add("hovering")
+
+  }, false);
+
+document.getElementById("umass").addEventListener("click", function() {
+    document.getElementById("collegeHampshire").className = "notVisible";
+    document.getElementById("hampshire").classList.remove("pageSelectTrue")
+    document.getElementById("hampshire").classList.add("hovering")
+
+    document.getElementById("collegeHolyoke").className = "visible";
+    document.getElementById("holyoke").classList.remove("pageSelectTrue")
+    document.getElementById("holyoke").classList.add("hovering")
+
+    document.getElementById("collegeSmith").className = "notVisible";
+    document.getElementById("smith").classList.remove("pageSelectTrue")
+    document.getElementById("smith").classList.add("hovering")
+
+    document.getElementById("collegeUMass").className = "notVisible";
+    document.getElementById("umass").classList.add("pageSelectTrue")
+    document.getElementById("umass").classList.remove("hovering")
+
+    document.getElementById("collegeAmherst").className = "notVisible";
+    document.getElementById("amherst").classList.remove("pageSelectTrue")
+    document.getElementById("amherst").classList.add("hovering")
 
   }, false);
